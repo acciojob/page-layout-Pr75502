b33d2ea2cefae8d997973c1d552ff282f2bb908c
prince prasad <pp1614582@gmail.com>
2025-04-14 12:42:54 +0530

    commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,11 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <header>
+        <h1>Welcome to my website</h1>
+      </header>
+      <p>This is the content of my website</p>
+      <footer><p>@ 2023 My Website. All rights reserved.</p></footer>
     </div>
   )
 }
